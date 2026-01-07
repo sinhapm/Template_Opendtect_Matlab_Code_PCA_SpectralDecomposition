5d154696536c1c814be89dbb86a9a691f1eb562e
Prashant M. Sinha <sinha.pm@gmail.com>
2026-01-08 02:12:28 +0530

    Update func_SpectralDecomposition.m

--- func_SpectralDecomposition.m
+++ func_SpectralDecomposition.m
@@ -1,6 +1,8 @@
 function [output]= func_SpectralDecomposition(tracedata,sampint,window_size,fhz)
 
  %% Write own code for Spectral Decomposition
-
+ %% Component of this program is blanked due to several reason
+ 
+ %% output = x + i * y
 
 end
